2dce3d50cc57f754985e759a5408129542ed0989
PaulaBuscacio <paula@buscacio.net>
2026-01-28 21:32:44 -0300

    inital hello

---
+++ src/main/java/net/buscacio/firstgithubactions/FirstGithubactionsApplication.java
@@ -0,0 +1,13 @@
+package net.buscacio.firstgithubactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstGithubactionsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FirstGithubactionsApplication.class, args);
+  }
+
+}
---
+++ src/test/java/net/buscacio/firstgithubactions/FirstGithubactionsApplicationTests.java
@@ -0,0 +1,13 @@
+package net.buscacio.firstgithubactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstGithubactionsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
